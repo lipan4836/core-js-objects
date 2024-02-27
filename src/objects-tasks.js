4f7b2cd9696e384ab39bd1f696fac14c71bb4ddb
Anton <lipan48@gmail.com>
2024-02-27 21:17:18 +0300

    feat: resume add solution, 15 pass

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -149,8 +149,13 @@ function makeWord(lettersObject) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  return (
+    queue.reduce(
+      (change, bill) => (bill > 25 ? change - bill : change + bill),
+      0
+    ) >= 0
+  );
 }
 
 /**
